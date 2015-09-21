f1b297a5c2f15c05dbe212272e9783369bfc3b7e
elujambio <eugeniolujambio@gmail.com>
2015-09-20 21:48:47 -0500

    Erased 3rd section, layout change on 5th
    Self descriptive.

--- src/js/global.js
+++ src/js/global.js
@@ -2,15 +2,15 @@ $(function(){
     var x = 0;
     var y = 0;
     var sect3 = document.getElementById('section-3');
-    var prueba = document.getElementById('prueba');    
-    var sect5 = document.getElementById('section-5');
+    // var prueba = document.getElementById('prueba');    
+    // var sect5 = document.getElementById('section-5');
 
     setInterval(function(){
         x+=1;
         y+=1;
         sect3.style.backgroundPosition =  x + 'px' + ' ' + y + 'px';
-        prueba.style.backgroundPosition =  x + 'px' + ' ' + -y + 'px';
-        sect5.style.backgroundPosition =  -x + 'px' + ' ' + -y + 'px';
+        // prueba.style.backgroundPosition =  x + 'px' + ' ' + -y + 'px';
+        // sect5.style.backgroundPosition =  -x + 'px' + ' ' + -y + 'px';
 
         if(x == 144){
           x=0;
